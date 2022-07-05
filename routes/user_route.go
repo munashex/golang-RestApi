2ee7656da7507e8bfc218b2ebf8859ef56abedb0
munashe madhuna <79254789+munashex@users.noreply.github.com>
2022-07-05 17:50:50 +0200

    Update user_route.go

--- routes/user_route.go
+++ routes/user_route.go
@@ -10,5 +10,5 @@ func UserRoute(app *fiber.App) {
     app.Get("/user/:userId", controllers.GetAUser)
     app.Put("/user/:userId", controllers.EditAUser)
     app.Delete("/user/:userId", controllers.DeleteAUser)
-    app.Get("/users", controllers.GetAllUsers)
+    app.Get("/users" controllers.GetAllUsers)
 }
